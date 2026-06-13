7cb25544b487d70e8f22a5e5c4bc26e111ca6dca
Al Farras <farrash320@gmail.com>
2026-06-13 17:54:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,139 @@
-function login(){
+// LOGIN
+function login() {
 
-let username =
-document.getElementById(
-"username"
-).value;
+    let username =
+    document.getElementById("username").value;
 
-let password =
-document.getElementById(
-"password"
-).value;
+    let password =
+    document.getElementById("password").value;
 
-if(
-username==="keluarga"
-&&
-password==="cctv2026"
-){
+    if (
+        username === "keluarga" &&
+        password === "cctv2026"
+    ) {
 
-window.location.href=
-"dashboard.html";
+        localStorage.setItem(
+            "login",
+            "true"
+        );
+
+        window.location.href =
+        "dashboard.html";
+
+    } else {
+
+        alert(
+            "Username atau password salah!"
+        );
+
+    }
 
 }
-else{
 
-alert(
-"Username atau password salah!"
+
+// CEK LOGIN
+function checkLogin() {
+
+    let login =
+    localStorage.getItem(
+        "login"
+    );
+
+    if (
+        login !== "true"
+    ) {
+
+        window.location.href =
+        "index.html";
+
+    }
+
+}
+
+
+// LOGOUT
+function logout() {
+
+    localStorage.removeItem(
+        "login"
+    );
+
+    window.location.href =
+    "index.html";
+
+}
+
+
+// UPDATE JAM
+function updateClock() {
+
+    let now = new Date();
+
+    let clock =
+    document.getElementById(
+        "clock"
+    );
+
+    let date =
+    document.getElementById(
+        "date"
+    );
+
+    if (clock) {
+
+        clock.innerHTML =
+        now.toLocaleTimeString(
+            "id-ID"
+        );
+
+    }
+
+    if (date) {
+
+        date.innerHTML =
+        now.toLocaleDateString(
+            "id-ID",
+            {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                year: "numeric"
+            }
+        );
+
+    }
+
+}
+
+
+// AUTO UPDATE
+setInterval(
+    updateClock,
+    1000
 );
-
-}
-
-}
-
-
-function logout(){
-
-window.location.href=
-"index.html";
-
-}
-
-
-function updateClock(){
-
-let now=new Date();
-
-let clock=
-document.getElementById(
-"clock"
-);
-
-let date=
-document.getElementById(
-"date"
-);
-
-if(clock){
-
-clock.innerHTML=
-now.toLocaleTimeString(
-"id-ID"
-);
-
-}
-
-if(date){
-
-date.innerHTML=
-now.toLocaleDateString(
-"id-ID",
-{
-weekday:"long",
-day:"numeric",
-month:"long",
-year:"numeric"
-}
-);
-
-}
-
-}
 
 updateClock();
 
-setInterval(
-updateClock,
-1000
-);
+
+// AUTO LOGIN
+if (
+    window.location.pathname.endsWith(
+        "index.html"
+    )
+) {
+
+    let login =
+    localStorage.getItem(
+        "login"
+    );
+
+    if (
+        login === "true"
+    ) {
+
+        window.location.href =
+        "dashboard.html";
+
+    }
+
+}
