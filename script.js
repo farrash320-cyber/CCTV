71b147dc6da76350e27232d1c333f8b23e645a35
Al Farras <farrash320@gmail.com>
2026-06-13 16:59:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,29 @@
-function openPage(id){
+function showPage(id){
 
-document
-.querySelectorAll(".page")
+document.querySelectorAll(".page")
 .forEach(page=>page.classList.remove("active"));
 
-document
-.getElementById(id)
+document.getElementById(id)
 .classList.add("active");
 
 }
 
+function updateClock(){
 
-function showLogin(){
+let now=new Date();
 
-openPage("loginPage");
+document.getElementById("clock")
+.innerHTML=now.toLocaleTimeString("id-ID");
 
-}
-
-
-function login(){
-
-let username =
-document.getElementById("username").value;
-
-let password =
-document.getElementById("password").value;
-
-
-if(
-username==="keluarga"
-&&
-password==="cctv2026"
-){
-
-openPage("dashboardPage");
-
-}
-else{
-
-alert("Username atau password salah!");
+document.getElementById("date")
+.innerHTML=now.toLocaleDateString("id-ID");
 
 }
 
-}
-
+setInterval(updateClock,1000);
 
 function logout(){
 
-document
-.getElementById("username")
-.value="";
-
-document
-.getElementById("password")
-.value="";
-
-openPage("homePage");
+location.reload();
 
 }
-
-
-
-function updateClock(){
-
-let now = new Date();
-
-document
-.getElementById("clock")
-.innerHTML=
-now.toLocaleTimeString("id-ID");
-
-document
-.getElementById("date")
-.innerHTML=
-now.toLocaleDateString(
-"id-ID",
-{
-weekday:"long",
-day:"numeric",
-month:"long",
-year:"numeric"
-}
-);
-
-}
-
-
-updateClock();
-
-setInterval(updateClock,1000);
