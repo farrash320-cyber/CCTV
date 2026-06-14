12c7d43b74c3e57c5fe5d73b423d35472667af1c
Al Farras <farrash320@gmail.com>
2026-06-14 11:32:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
 function login(){
 
+    let username =
+    document.getElementById(
+    "username").value;
+
+    let password =
+    document.getElementById(
+    "password").value;
+
     let button =
     document.getElementById(
-        "loginButton"
-    );
+    "loginButton");
 
     button.innerHTML =
     "Masuk...";
 
-
-
     setTimeout(function(){
 
-        let username =
-        document.getElementById(
-            "username"
-        ).value;
-
-        let password =
-        document.getElementById(
-            "password"
-        ).value;
-
         if(
-
-            username==="keluarga"
-
-            &&
-
-            password==="cctv2026"
-
+        username==="keluarga"
+        &&
+        password==="cctv2026"
         ){
 
             localStorage.setItem(
-                "login",
-                "true"
-            );
+            "login",
+            "true");
 
-            alert(
-                "Selamat datang kembali!"
-            );
-
-            window.location.href =
+            window.location.href=
             "dashboard.html";
 
         }
@@ -49,10 +35,9 @@ function login(){
         else{
 
             alert(
-                "Username atau password salah!"
-            );
+            "Username atau password salah!");
 
-            button.innerHTML =
+            button.innerHTML=
             "Masuk";
 
         }
@@ -66,104 +51,11 @@ function login(){
 function handleKey(event){
 
     if(
-        event.key==="Enter"
+    event.key==="Enter"
     ){
 
         login();
 
     }
-
-}
-
-
-
-function checkLogin(){
-
-    if(
-
-        localStorage.getItem(
-            "login"
-        )!=="true"
-
-    ){
-
-        window.location.href =
-        "index.html";
-
-    }
-
-}
-
-
-
-function logout(){
-
-    localStorage.removeItem(
-        "login"
-    );
-
-    window.location.href =
-    "index.html";
-
-}
-
-
-
-function updateClock(){
-
-    let now =
-    new Date();
-
-    let clock =
-    document.getElementById(
-        "clock"
-    );
-
-    let date =
-    document.getElementById(
-        "date"
-    );
-
-    if(clock){
-
-        clock.innerHTML =
-        now.toLocaleTimeString(
-            "id-ID"
-
-        );
-
-    }
-
-    if(date){
-
-        date.innerHTML =
-        now.toLocaleDateString(
-            "id-ID",
-            {
-                weekday:"long",
-                day:"numeric",
-                month:"long",
-                year:"numeric"
-            }
-
-        );
-
-    }
-
-}
-
-updateClock();
-
-setInterval(
-    updateClock,
-    1000
-);
-
-
-
-function openV360(){
-
-    window.location.href =
-    "v360pro://";
 
 }
